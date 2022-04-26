878012d83aa9f48bc09604d11b0fae7ddb560f53
lstasp <luisvaley@hotmail.com>
2022-04-26 19:25:54 -0400

    commit 2

--- src/main/java/com/school/students/demo/jpaschool/repositories/StudentRepository.java
+++ src/main/java/com/school/students/demo/jpaschool/repositories/StudentRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student, Integer> {
-    List<Student> findByNameIgnoreCase(String name);
+    List<Student>  findByNameIgnoreCase(String name);
 }
